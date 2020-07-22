96a2a55b6fb4aafc99a84dc31647734672f8a5bf
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-07-22 20:54:19 +0000

    Different way of doing ListView, in preparation for re-ordering stores based on distance from device.

--- lib/main.dart
+++ lib/main.dart
@@ -104,22 +104,20 @@ class _MainPageState extends State<MainPage> {
             children: <Widget>[
               // TODO: Controls.
               Expanded(
-                child: ListView.separated(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    final dealer = snapshot.data[index];
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(dealer.image_url),
+                child: ListView(
+                  children: ListTile.divideTiles(
+                    context: context,
+                    tiles: [
+                      for (var dealer in snapshot.data) ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(dealer.image_url),
+                        ),
+                        title: Text(dealer.name),
+                        subtitle: Text(dealer.is_open ? 'Open'.i18n : 'Closed'.i18n),
                       ),
-                      title: Text(dealer.name),
-                      subtitle: Text(dealer.is_open ? 'Open'.i18n : 'Closed'.i18n),
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return Divider();
-                  },
-                )
+                    ],
+                  ).toList(),
+                ),
               ),
             ],
           );
