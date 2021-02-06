a813b690a7f2865bcd094ae93969bc457fb46453
Helgi Hrafn Gunnarsson <helgi@binary.is>
2021-02-06 16:52:25 +0000

    JSON example generator moved to scripts directory and made to only run from project root.

--- scripts/generate_example.py
+++ scripts/generate_example.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 # Generates a FILENAME, named after today (i.e. "2021-01-05.json" with the
 # prettified JSON output of the remote store data.
+#
+# Should be run from the project root.
+import os
 import requests
 import json
 from datetime import datetime
 
+DEST_DIR = 'example-data'
+
+if not os.path.exists(DEST_DIR):
+    print('Error: Must be run from project root, like so: scripts/generate_example.py.')
+    quit(1)
+
 URL = 'https://www.vinbudin.is/addons/origo/module/ajaxwebservices/search.asmx/GetAllShops'
-FILENAME = '%s.json' % datetime.now().strftime('%Y-%m-%d')
+FILENAME = '%s/%s.json' % (DEST_DIR, datetime.now().strftime('%Y-%m-%d'))
 
 print('Fetching data...', end='', flush=True)
 response = requests.get(URL, headers={'Content-Type': 'application/json; charset=utf-8'})
